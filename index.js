55643f3e5df5eb73786684550c087f5b1a5980eb
Deonisteatr <144840698+Deonisteatr@users.noreply.github.com>
2024-04-23 11:15:10 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,7 @@
+import { slider } from "./src/js/slider.js";
+import { onloading } from "./src/js/onloading.js";
+
+import styles from "./style.scss";
+
+slider();
+onloading();
---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+let path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require("copy-webpack-plugin");
+//const ESLintPlugin = require('eslint-webpack-plugin');
+
+module.exports = {
+    entry: path.resolve(__dirname, './index.js'),
+    output: {
+        path: path.resolve(__dirname, 'output'),
+        filename: 'main.js'
+    },
+    mode: 'production',
+    plugins: [new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin(
+            {
+                template: './src/html/index.pug',
+                filename: 'index.html'
+            }
+        ),
+        new CopyPlugin({
+            patterns: [
+              { from: "./src/img", to: "img" },
+            ],
+          }),
+        /*new ESLintPlugin({
+            fix: true
+          }),*/
+],
+    module: {
+        rules: [
+          {
+            test: /\.scss$/i,
+            use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+          },
+          {
+            test: /\.pug$/,
+            loader: 'pug-loader',
+            options: {
+            pretty: true
+            }
+        }
+        ],
+     
+    },
+    optimization: {
+        minimizer: [
+          `...`,
+          new CssMinimizerPlugin(),
+        ],
+      },
+      
+}; 
